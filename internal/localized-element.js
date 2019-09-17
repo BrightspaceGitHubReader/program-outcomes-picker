ddc9e6a35855b00dd0830947c7a86a7127d89bc9
Matt Pharoah <Matt.Pharoah@d2l.com>
2019-09-17 13:49:10 -0400

    Use LocalizeMixin from Brightspace Core for localization

--- internal/localized-element.js
+++ internal/localized-element.js
@@ -1,25 +1,57 @@
 import { LitElement } from 'lit-element/lit-element.js';
-import { CurrentLanguage } from './language.js';
+import { LocalizeMixin } from '@brightspace-ui/core/mixins/localize-mixin.js';
 
-class LocalizedLitElement extends LitElement {
+import ar from '../lang/ar.js';
+import de from '../lang/de.js';
+import en from '../lang/en.js';
+import es from '../lang/es.js';
+import fr from '../lang/fr.js';
+import ja from '../lang/ja.js';
+import ko from '../lang/ko.js';
+import nb from '../lang/nb.js';
+import nl from '../lang/nl.js';
+import pt from '../lang/pt.js';
+import sv from '../lang/sv.js';
+import tr from '../lang/tr.js';
+import zh from '../lang/zh.js';
+import zhTW from '../lang/zh-TW.js';
+
+const FALLBACK_LANGUAGE = 'en';
+const translations = {
+	ar: ar,
+	de: de,
+	en: en,
+	es: es,
+	fr: fr,
+	ja: ja,
+	ko: ko,
+	nb: nb,
+	nl: nl,
+	pt: pt,
+	sv: sv,
+	tr: tr,
+	zh: zh,
+	['zh-cn']: zh,
+	['zh-tw']: zhTW
+};
+
+class LocalizedLitElement extends LocalizeMixin( LitElement ) {
 	
-	constructor() {
-		super();
-		this._onLanguageChanged = this.performUpdate.bind( this );
-	}
-	
-	connectedCallback() {
-		CurrentLanguage.addChangeListener( this._onLanguageChanged );
-		super.connectedCallback();
-	}
-	
-	disconnectedCallback() {
-		CurrentLanguage.removeChangeListener( this._onLanguageChanged );
-		super.disconnectedCallback();
-	}
-	
-	localize( term, options ) {
-		return CurrentLanguage.localize( term, options );
+	static async getLocalizeResources( langs ) {
+		let resolvedLanguage = FALLBACK_LANGUAGE;
+		const languageDocument = Object.assign( {}, translations[FALLBACK_LANGUAGE] );
+		
+		langs.reverse().forEach( lang => {
+			if( translations[lang] ) {
+				resolvedLanguage = lang;
+				Object.assign( languageDocument, translations[lang] );
+			}
+		});
+		
+		return {
+			language: resolvedLanguage,
+			resources: languageDocument
+		};
 	}
 	
 }
